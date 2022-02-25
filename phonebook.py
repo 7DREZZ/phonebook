9dd0e6029ac11ed8d5def241671b6109e0fd1ba6
7DREZZ <99985897+7DREZZ@users.noreply.github.com>
2022-02-25 16:03:24 +0300

    second commit
    finished version

---
+++ phonebook.py
@@ -0,0 +1,42 @@
+import argparse
+book = {"Masha": 89778881539, "Pasha": 89778081476, "Natasha": 89771231536}
+
+parser = argparse.ArgumentParser(description='Telephon book')
+parser.add_argument('-a', "--add", dest="param1")
+parser.add_argument('-d', "--delete", dest="param2")
+parser.add_argument('-s', "--show", dest="param3", default="all")
+
+args = parser.parse_args()
+
+if args.param1:
+    name, tele = args.param1.split(":")
+    if name in book:
+        book[name] = [book.get(name), int(tele)]
+        print("Контакт ", name, " обновлен")
+        print(name, ":", book[name])
+    else:
+        book[name] = int(tele)
+        print("Контакт", name, " добавлен")
+        print(name, ":", book[name])
+
+elif args.param2:
+    name = args.param2
+    if name in book:
+        print("Контакт ", name, " удален")
+        del book[name]
+        print(book)
+    elif name == "all":
+        print("Телефонная книга очищена")
+        book.clear()
+        print(book)
+    else:
+        print("Ошибка, контакт ", name, " не найден")
+
+elif args.param3:
+        name = args.param3
+        if name in book:
+            print(book.get(name, 0))
+        elif name == "all":
+            print(book)
+        else:
+            print("Ошибка, контакт ", name, " не найден")
